1a8fc50555f1a303737f9f175272350bf501e795
andrebelluci <andrebelluci@gmail.com>
2025-08-25 09:26:58 -0300

    bug fix getBasePath

--- public/assets/js/dark-mode-three.js
+++ public/assets/js/dark-mode-three.js
@@ -36,9 +36,11 @@ class DarkModeThreeJS {
   }
 
   getBasePath() {
-    const path = window.location.pathname;
-    const depth = (path.match(/\//g) || []).length - 1;
-    return depth > 0 ? '../'.repeat(depth) : './';
+    if (window.location.hostname.includes("github.io")) {
+      return "/dharmalabs/public/";
+    } else {
+      return "./"; // ou o que você usa no ambiente local
+    }
   }
 
   getResponsiveConfig() {
